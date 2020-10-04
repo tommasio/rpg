1596e802adde5fba9c30ac515436b49bdf3e1d33
tommy <tommypeled10@gmail.com>
2020-10-04 19:55:38 +0200

    dd

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,6 +6,9 @@ var velocity = Vector2.ZERO
 const FRINCTION = 500
 const ACCELERATIOM = 500
 const MAX_SPEED = 100 
+
+onready var animationPlayer = $AnimationPlayer
+
  
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -14,8 +17,17 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animationPlayer.play("RunRight")
+		elif input_vector.x < 0:
+			animationPlayer.play("RunLeft")
+		elif input_vector.y < 0:
+			animationPlayer.play("RunUp")
+		elif input_vector.y > 0:
+			animationPlayer.play("RunDown")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATIOM * delta)
 	else:
+		animationPlayer.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRINCTION * delta)
 	
 	velocity = move_and_slide(velocity)
